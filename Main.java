580ed23b1e40e16a43a145926b3461c15efb0ae5
harish <harisht1320@gmail.com>
2025-07-22 19:14:57 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simulator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(new Panel());
+        frame.pack(); 
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
