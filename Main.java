1c4e55d391ac4013d5c6aec1ae9eb57593e5baf3
harish <harisht1320@gmail.com>
2025-07-22 19:46:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Simulator");
+        JFrame frame = new JFrame("Ghost Companion");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.add(new Panel());
-        frame.pack(); 
+        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
